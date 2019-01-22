c5764cb3af8a1d6600edc534c6cf8f5501091b77
RoyCsuka <roy-csuka@live.nl>
2019-01-22 08:30:48 +0100

    Add files via upload
    Responsive maken, nu begin voor iPad op de home pagina.

--- js/open-filters.js
+++ js/open-filters.js
@@ -1,11 +1,10 @@
 var filterButton = document.querySelector('section button:first-of-type');
 var overlayFilters = document.querySelector('div section form:first-of-type');
+var terugButton = document.querySelector('div section form:first-of-type a');
 
 filterButton.addEventListener('click', function(){
     overlayFilters.classList.add('toon-filters') /*zet class erbij of haalt er weg*/
 })
-
-var terugButton = document.querySelector('div section form:first-of-type a');
 
 terugButton.addEventListener('click', function(){
     overlayFilters.classList.remove('toon-filters') /*haalt class weg*/
